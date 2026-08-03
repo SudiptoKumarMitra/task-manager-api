bd26c4789dbb818264b473b9487ccc367d1030fb
SudiptoKumarMitra <mitra15-6104@s.diu.edu.bd>
2026-08-03 22:25:16 +0600

    Handle HTTP methods for tasks endpoint

--- main.go
+++ main.go
@@ -10,7 +10,18 @@ func healthHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintln(w, "Server is healthy")
 }
 func taskHandler( w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w, "No tasks available")
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Fprintln(w, "Task List")
+	case http.MethodPost:
+		fmt.Fprintln(w, "Task Created")
+	case http.MethodPut:
+		fmt.Fprintln(w, "Task Updated")
+	case http.MethodDelete:
+		fmt.Fprintln(w, "Task Deleted")
+	default:
+		http.Error(w,"Method Not allowd", http.StatusMethodNotAllowed)
+	}
 }
 func main() {
 	http.HandleFunc("/",homeHandler)
