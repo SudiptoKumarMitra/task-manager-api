7129f324f4cb9eaf59853507a291ebc607f54606
SudiptoKumarMitra <mitra15-6104@s.diu.edu.bd>
2026-08-04 10:31:49 +0600

    Add task creation and search by id

--- main.go
+++ main.go
@@ -2,33 +2,78 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
+	"strconv"
 )
+type User struct {
+	ID int `json:"id"`
+	Name string `json:"name"`
+	Age int `json:"age"`
+}
+type Task struct {
+	ID int `json:"id"`
+	Title string `json:"title"`
+}
+var Tasks = []Task{}
+var nextID = 1
 func homeHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintln(w, "Welcome to Task Manager API")
 }
 func healthHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintln(w, "Server is healthy")
 }
-func usersHandle(w http.ResponseWriter, r *http.Request){
+func usersHandler(w http.ResponseWriter, r *http.Request){
+	
 	switch r.Method {
 		case http.MethodGet:
-			fmt.Fprintln(w,"Users List")
+			fmt.Fprintln(w, "User List")
 		case http.MethodPost:
-			fmt.Fprintln(w,"User Created")
+			fmt.Fprintln(w, "User Created")
 		case http.MethodPut:
-			fmt.Fprintln(w,"User Updated")
+			fmt.Fprintln(w, "User Updated")
 		case http.MethodDelete:
-			fmt.Fprintln(w,"User Deleted")
+			fmt.Fprintln(w, "User Deleted")
 		default:
 			http.Error(w,"Method Not Allowed",http.StatusMethodNotAllowed)
 	}
 }
-func taskHandler( w http.ResponseWriter, r *http.Request){
+func taskHandler(w http.ResponseWriter, r *http.Request){
 	switch r.Method {
 	case http.MethodGet:
-		fmt.Fprintln(w, "Task List")
+		if r.URL.Query().Get("id") != "" {
+			id, err := strconv.Atoi(r.URL.Query().Get("id"))
+			if err != nil {
+				http.Error(w, "Invalid ID", http.StatusBadRequest)
+				return
+			}
+			if id > len(Tasks) || id < 1 {
+				http.Error(w, "Task Not Found", http.StatusNotFound)
+				return
+			}
+			for _, task := range Tasks {
+				if task.ID == id {
+					w.Header().Set("Content-Type", "application/json")
+					json.NewEncoder(w).Encode(task)
+					return
+				}
+			}
+		} else {
+			w.Header().Set("Content-type", "application/json")
+			json.NewEncoder(w).Encode(Tasks)
+		}
 	case http.MethodPost:
-		fmt.Fprintln(w, "Task Created")
+		var task Task
+		err := json.NewDecoder(r.Body).Decode(&task)
+		if err != nil{
+			http.Error(w, "Invalid JSON", http.StatusBadRequest)
+			return
+		}
+		task.ID = nextID
+		nextID++
+		Tasks=append(Tasks,task)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(task)
 	case http.MethodPut:
 		fmt.Fprintln(w, "Task Updated")
 	case http.MethodDelete:
@@ -41,7 +86,7 @@ func main() {
 	http.HandleFunc("/",homeHandler)
 	http.HandleFunc("/health",healthHandler) 
 	http.HandleFunc("/tasks",taskHandler)
-	http.HandleFunc("/users",usersHandle)
+	http.HandleFunc("/users",usersHandler)
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
