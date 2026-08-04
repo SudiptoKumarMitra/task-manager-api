9b89347b1220030a840bea14b46c479be4f6af1b
SudiptoKumarMitra <mitra15-6104@s.diu.edu.bd>
2026-08-04 06:44:26 +0600

    Add Users endpoint

--- main.go
+++ main.go
@@ -9,6 +9,20 @@ func homeHandler(w http.ResponseWriter, r *http.Request){
 func healthHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintln(w, "Server is healthy")
 }
+func usersHandle(w http.ResponseWriter, r *http.Request){
+	switch r.Method {
+		case http.MethodGet:
+			fmt.Fprintln(w,"Users List")
+		case http.MethodPost:
+			fmt.Fprintln(w,"User Created")
+		case http.MethodPut:
+			fmt.Fprintln(w,"User Updated")
+		case http.MethodDelete:
+			fmt.Fprintln(w,"User Deleted")
+		default:
+			http.Error(w,"Method Not Allowed",http.StatusMethodNotAllowed)
+	}
+}
 func taskHandler( w http.ResponseWriter, r *http.Request){
 	switch r.Method {
 	case http.MethodGet:
@@ -20,13 +34,14 @@ func taskHandler( w http.ResponseWriter, r *http.Request){
 	case http.MethodDelete:
 		fmt.Fprintln(w, "Task Deleted")
 	default:
-		http.Error(w,"Method Not allowd", http.StatusMethodNotAllowed)
+		http.Error(w,"Method Not allowed", http.StatusMethodNotAllowed)
 	}
 }
 func main() {
 	http.HandleFunc("/",homeHandler)
-	http.HandleFunc("/health",healthHandler)
+	http.HandleFunc("/health",healthHandler) 
 	http.HandleFunc("/tasks",taskHandler)
+	http.HandleFunc("/users",usersHandle)
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
